5b3f00d06c1881d87c47dd8e3967cb785bbd200c
RozlinA <144224291+RozlinA@users.noreply.github.com>
2025-03-29 19:35:02 +0100

    Implement header in Layout and add content to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
+import backgroundImage from "../assets/movie-posters.jpg";
+
 export const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <section className="w-screen h-screen flex items-center justify-center">
+      <div
+        className="w-full h-full bg-cover bg-center flex flex-col justify-center items-center"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh", // Se till att hela skärmen fylls
+        }}
+      >
+        <div className="w-[80%] md:w-[45%] text-white flex flex-col gap-6">
+          <h1 className="text-4xl md:text-7xl font-bold">
+            All your favourite movies in one place.
+          </h1>
+          <h2 className="text-2xl md:text-3xl">
+            Search, save and start watching!
+          </h2>
+          <div className="flex flex-col md:flex-row w-full md:bg-white md:rounded-full overflow-hidden gap-3">
+            <input
+              type="text"
+              placeholder="Search movie..."
+              className="md:flex-grow p-3 pl-5 outline-none text-black bg-white rounded-full"
+            />
+            <button className="bg-red-500 text-white px-6 py-3 rounded-full md:-ml-2 cursor-pointer">
+              SEARCH
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,75 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const Layout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
+      <header
+        className={`fixed top-0 left-0 w-full z-20 ${
+          isMenuOpen ? "bg-black" : "bg-transparent"
+        }`}
+      >
+        <nav className="flex justify-between px-4 md:px-10 items-center h-20 text-lg">
+          <div className="flex items-center">
+            <NavLink
+              to={"/"}
+              className="text-2xl font-bold hover:text-gray-500 text-white"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              MOVIE VAULT
+            </NavLink>
+          </div>
+
+          <div className="md:hidden">
+            <button onClick={handleToggleMenu}>
+              <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="2x" />
+            </button>
+          </div>
+
+          <ul className="hidden md:block md:flex md:justify-around md:w-full md:max-w-md font-medium text-white">
+            <li className="hover:text-gray-300">
+              <NavLink to={"/"}>HOME</NavLink>
+            </li>
+            <li className="hover:text-gray-300">
+              <NavLink to={"/movies"}>MOVIES</NavLink>
+            </li>
+            <li className="hover:text-gray-300">
+              <NavLink to={"/watchlist"}>WATCHLIST</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Mobil meny */}
+
+      {isMenuOpen && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black text-white z-10">
+          <nav className="flex flex-col items-center justify-center h-full">
+            <ul className="flex-col text-xl">
+              <li className="p-4" onClick={handleToggleMenu}>
+                <NavLink to={"/"}>HOME</NavLink>
+              </li>
+
+              <li className="p-4" onClick={handleToggleMenu}>
+                <NavLink to={"/movies"}>MOVIES</NavLink>
+              </li>
+
+              <li className="p-4" onClick={handleToggleMenu}>
+                <NavLink to={"/watchlist"}>WATCHLIST</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+
       <main>
         <Outlet />
       </main>
